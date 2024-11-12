b67af9bb0113a2b7f0458c29347fd718ed3a3e6d
unknown <Administrador@LAB4413848.fuvs.br>
2024-11-12 19:26:55 -0300

    gerar e validar token

--- autenticacao/src/routes/usuario.js
+++ autenticacao/src/routes/usuario.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const usuarioSchema = require("../schemas/usuario")
 const encriptador = require("../services/EncriptadorDeSenhas")
 const {Usuario} = require("../models")
+const jwt = require("jsonwebtoken")
 
 router.post('/cadastro', async (req, res) => {
     // extrair os dados
@@ -26,5 +27,79 @@ router.post('/cadastro', async (req, res) => {
         res.status(400).send(err.errors || err.message)
     }
 })
+
+// gerarToken
+router.post('/gerarToken', async (req, res) => {
+    const {email, senha} = req.body
+
+    if(!email || !senha){
+        return res.status(403).send({
+            mensagem: "Usuário ou senha inválidos",
+            sucesso: false
+        })
+    }
+
+    const usuario = await Usuario.findOne({
+        where: {
+            email: email
+        }
+    })
+
+    // se a variável usuário for algum valor vazio (null, undefined)
+    if(!usuario){
+        return res.status(403).send({
+            mensagem: "Usuário ou senha inválidos",
+            sucesso: false
+        })
+    }
+
+    const senhaValida = await encriptador.comparar(usuario.senha, senha)
+
+    if(!senhaValida){
+        return res.status(403).send({
+            mensagem: "Usuário ou senha inválidos",
+            sucesso: false
+        })
+    }
+
+    const payload = {
+        dados: {
+            email: usuario.email,
+            nome: usuario.nome
+        }
+    }
+
+    const token = jwt.sign(payload, process.env.KEY, {
+        algorithm: 'HS256',
+        expiresIn: 60*20 // 20 minutos
+    })
+
+    return res.status(200).send({
+        sucesso: true,
+        token: token
+    })
+})
+
+// validarToken
+router.post('/validarToken', async (req, res) => {
+    const {token} = req.body
+
+    try{
+        const decodificado = jwt.verify(token, process.env.KEY)
+
+        return res.send({
+            sucesso: true,
+            mensagem: "Token Válido",
+            dados: decodificado
+        })
+    }catch(err){
+        return res.status(401).send({
+            sucesso: false,
+            mensagem: err.message
+        })
+    }
+})
+
+// redefinirSenha
 
 module.exports = router
